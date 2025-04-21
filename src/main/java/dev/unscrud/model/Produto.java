5a507fab1e9c9deb6d53086bbc02297b82154a98
unscrud <danieldevop@gmail.com>
2025-04-21 07:40:50 -0300

    Classe de modelo para Produto

---
+++ src/main/java/dev/unscrud/model/Produto.java
@@ -0,0 +1,51 @@
+package dev.unscrud.model;
+
+import java.math.BigDecimal;
+
+public class Produto {
+  private int id;
+  private String name;
+  private String description;
+  private BigDecimal price;
+  private String category;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+}
