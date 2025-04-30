bf53a659715440c6f7ad63ba13d77524f59a34bf
unscrud <danieldevop@gmail.com>
2025-04-30 20:54:35 -0300

    FileReader funciona localmente, nas foi necessário usar o InputStream dentro do container

--- src/main/java/dev/unscrud/Main.java
+++ src/main/java/dev/unscrud/Main.java
@@ -5,15 +5,21 @@ import dev.unscrud.model.Produto;
 import dev.unscrud.service.TraduzirProdutoService;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) throws FileNotFoundException {
-    List<Produto> produtos = new CsvToBeanBuilder(new FileReader("src/main/resources/products.csv"))
-            .withType(Produto.class).build().parse();
+    InputStream is = Main.class.getClassLoader().getResourceAsStream("products.csv");
+    if (is == null) {
+      throw new FileNotFoundException("Arquivo 'products.csv' não encontrado no classpath.");
+    }
+    InputStreamReader reader = new InputStreamReader(is);
+
+    List<Produto> produtos = new CsvToBeanBuilder(reader).withType(Produto.class).build().parse();
 
     produtos.forEach(System.out::println);
 
